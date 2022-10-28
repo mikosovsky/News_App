9c8057d7b5d6645681114a588b6a095fc13b06a6
mikosovsky <mikolaj.dawczyk@gmail.com>
2022-10-28 21:56:42 +0200

    Create repo

---
+++ News App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News App
+//
+//  Created by Mikołaj Dawczyk on 28/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
