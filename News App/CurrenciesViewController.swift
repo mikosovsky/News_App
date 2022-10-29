23e68fdac4ab93b922f723403c6baf71570087c2
mikosovsky <mikolaj.dawczyk@gmail.com>
2022-10-29 23:15:45 +0200

    Create ViewControllers Clases

---
+++ News App/CurrenciesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CurrenciesViewController.swift
+//  News App
+//
+//  Created by Mikołaj Dawczyk on 29/10/2022.
+//
+
+import UIKit
+
+class CurrenciesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
