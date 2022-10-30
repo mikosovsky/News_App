88c4335bcd800ac742a983c30ce9bb1bce23dc16
mikosovsky <mikolaj.dawczyk@gmail.com>
2022-10-30 19:01:48 +0100

    Changes in News View and prepare template for NewsViewController

--- News App/Controll/NewsViewController.swift
+++ News App/Controll/NewsViewController.swift
@@ -9,12 +9,43 @@ import UIKit
 
 class NewsViewController: UIViewController {
     
+    @IBOutlet weak var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        searchTextField.delegate = self
+        
     }
-    
     
 }
 
+//MARK: - UITextFieldDelegate
+
+extension NewsViewController: UITextFieldDelegate {
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let phrase = textField.text {
+            
+        }
+        textField.text = ""
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            return false
+        }
+    }
+}
