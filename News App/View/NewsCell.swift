68dad9083751699589bf5ef254d20cfceda080ae
mikosovsky <mikolaj.dawczyk@gmail.com>
2022-11-07 21:54:41 +0100

    Fix of bugging text in cell

--- News App/View/NewsCell.swift
+++ News App/View/NewsCell.swift
@@ -12,11 +12,14 @@ class NewsCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var articleImage: UIImageView!
     @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var stackView: UIStackView!
+    
+    @IBOutlet weak var cellView: UIView!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        stackView.layer.cornerRadius = 10
+        cellView.layer.cornerRadius = 10
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
